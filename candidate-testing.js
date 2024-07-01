94349f2e2c32fee69fd6e869f5a279f600b5f29f
remi <rachelmilb89@gmail.com>
2024-07-01 00:58:11 -0400

    FULL ASSIGNMENT COMPLETE UPDATED

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,14 +1,17 @@
 const input = require('readline-sync');
 
 // TODO 2: modify your quiz app to ask 5 questions //
-
 // TODO 1.1a: Define candidateName //Declare this variable as an empty string
 let candidateName = "";
+
+
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
+
+
 
 
 //TODO: Variables for Part 2
@@ -33,44 +36,64 @@ let candidateAnswers = [];
 
 
 
+
+
+
+
+
 // TODO 1.1b: Ask for candidate's name //
 function askForName() {
-  candidateName = input.question("Please enter your name: ");
+  candidateName = input.question("Candidate Name: ");
   
 }
 
+
+
+
+
+
+
 // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 function askQuestion() {
+
   let answer = "";
+  let questionNumber = 1
     //candidateAnswer = input.question(question);
+
     for (let i = 0; i < questions.length; i++) {//loops through the array
-      answer = input.question(questions[i]);//variable for input of questions at i
+      answer = input.question(questionNumber + ") " + questions[i]);//variable for input of questions at i
       candidateAnswers.push(answer);//put the answers in the empty array
+      console.log("Your Answer: " + candidateAnswers[i]);
+      console.log("Correct Answer: " + correctAnswers[i]);
+      console.log();
+      questionNumber += 1
     }
     
 }
 
-function gradeQuiz(candidateAnswers) {
 
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+
+
+
+
+
+function gradeQuiz(candidateAnswers) {
+// TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
 /*if (candidateAnswer === correctAnswer){ V1 TRY NOT CORRECT****
     console.log("Your Answer:");
 } else {
     console.log("YOU MESSED UP!");
 }
-
-
  //V1 TRY UPDATED INFO WITH THIS BETTER CODE THAT WORKS
 console.log(`Your Answer: ${candidateAnswer}`);
 console.log(`Correct Answer: ${correctAnswer}`);
 */
-console.log();
+// console.log();
 
+// console.log(`Your Answer: ${candidateAnswers}`);
+// console.log(`Correct Answer: ${correctAnswers}`);
 
-
-console.log(`Your Answer: ${candidateAnswers}`);
-console.log(`Correct Answer: ${correctAnswers}`);
 
 
 
@@ -78,23 +101,17 @@ console.log(`Correct Answer: ${correctAnswers}`);
   let numberOfCorrectAnswers = 0
   let numberOfQuestions = 5
 
-  console.log();
+  
 // console.log(candidateAnswers);
 // console.log(correctAnswers);
-
-console.log();
-
 // if (`${candidateAnswers[0]} === ${correctAnswers[0]}`) {
 //   numberOfCorrectAnswers += 1
 //   console.log(numberOfCorrectAnswers);
 // }
 
+
 for (let i = 0; i < candidateAnswers.length; i++) {
-
   let lowerCaseCandidateAnswer = candidateAnswers[i].toLowerCase();//CREATE VARIABLE TO REPRESENT ARRAY[I] CHANGED TO LOWERCASE
-  
-  // let lowerCaseCorrectAnswers = correctAnswers.toLowerCase();
-
 
   if (lowerCaseCandidateAnswer === correctAnswers[i].toLowerCase()) {//CHANGE ARRAY INDEX TO LOWERCASE HERE
     // console.log("candidateAnswers[i]",candidateAnswers[i]);
@@ -108,30 +125,23 @@ for (let i = 0; i < candidateAnswers.length; i++) {
 }
 
 grade = (numberOfCorrectAnswers / numberOfQuestions) * 100
-//Convey to the candidate if they have passed the quiz with an 80% or if they have failed
 
-console.log(grade + "/" + "100");
 if (grade >= 80) {
-  console.log(`>>> Overall Grade: ${grade} % (${numberOfCorrectAnswers} of ${numberOfQuestions} responses correct) <<<`)
+  console.log(`>>> Overall Grade: ${grade}% (${numberOfCorrectAnswers} of ${numberOfQuestions} responses correct) <<<`);
+  console.log(">>> Status: PASSED <<<");
 } else {
-    console.log(">>> Status: FAILED <<<")
+  console.log(`>>> Overall Grade: ${grade}% (${numberOfCorrectAnswers} of ${numberOfQuestions} responses correct) <<<`);
+  console.log(">>> Status: FAILED <<<")
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   return grade;
 }
+
+
+
+
+
+
+
 
 function runProgram() {
   askForName();
@@ -140,6 +150,18 @@ function runProgram() {
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
+
+//To DO: ask question + show your answer + show correct answer
+//show overall grade result
+//show status
+
+
+
+
+
+
+
+
 
 // ----------- Don't write any code or change any code below this line ---------- //
 module.exports = {
